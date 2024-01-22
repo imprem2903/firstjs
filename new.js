bba1f0f58f6cd7168db0405182b4c9b3a536fe59
Prem Malhotra <malhotra.prem290903@gmail>
2024-01-22 23:45:40 +0530

    new update

--- new.js
+++ new.js
@@ -1,1 +1,31 @@
-console.log("Hello!")
+// console.log(3+3)
+// console.log("prem")
+
+// let name = "prem"
+// let age = 19
+// let isLoggedIN = false
+
+
+// console.log(typeof "name")
+// console.log(typeof age)
+// console.log(typeof Object)
+// console.log(typeof null)
+
+// let score = "undefined"
+// console.log(typeof(score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// let isLoggedIN = 0
+
+// let booleanIsLoggedIn = Boolean(isLoggedIN)
+// console.log(booleanIsLoggedIn)
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber)
+console.log(typeof someNumber)
