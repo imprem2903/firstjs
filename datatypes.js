ab7184b94d94d593806f086c0f69d184f90a1488
Prem Malhotra <malhotra.prem290903@gmail>
2024-01-24 01:30:35 +0530

    Update

--- datatypes.js
+++ datatypes.js
@@ -3,14 +3,14 @@
 // Primitive (Call by Values) / Reference type :
 //      String, Number, Boolean, Null,  Undefined, Symbol, BigInt
 
-const score = 100
-const scoreValue = 100.2
-const isLoggedIn = false   
-let userEmail;
+// const score = 100
+// const scoreValue = 100.2
+// const isLoggedIn = false   
+// let userEmail;
 
-const id = Symbol('123')
-const anotheID = Symbol('123')
-console.log(id === anotheID)
+// const id = Symbol('123')
+// const anotheID = Symbol('123')
+// console.log(id === anotheID)
 
 
      
@@ -18,14 +18,37 @@ console.log(id === anotheID)
 // Non Primitive (Reference Type ) :
 //     Array, Objects, Functions
 
-const drugs = ["Mdma", "Cocaine"];
-{
-  name: "Prem malhotra";
-  age: 20;
+// const drugs = ["Mdma", "Cocaine"];
+// {
+//   name: "Prem malhotra";
+//   age: 20;
+// }
+
+// const myFunction = 
+// function(){
+//     console.log("Hello World!")
+// }
+// console.log(drugs)
+
+// *********************************************
+// Stack(Primitive), Heap(Non-Primitive Memory)
+
+// let myName = "PreMalhotra"
+// let newName = myName
+// newName = "Malhotra Prem"
+
+// console.log(myName)
+// console.log(newName)
+
+let userOne = {
+    email: "user@gmail.com",
+    upi: "user@ybl"
 }
 
-const myFunction = 
-function(){
-    console.log("Hello World!")
-}
-console.log(drugs)
+
+let userTwo = userOne
+
+userTwo.email = "user123@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
