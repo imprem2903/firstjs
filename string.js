806d794535a71237585f57645b9ff4f2337c8f09
Prem Malhotra <malhotra.prem290903@gmail>
2024-01-24 13:17:27 +0530

    New

---
+++ string.js
@@ -0,0 +1,24 @@
+// const myName = "Prem Malhotra"
+// const repCount = 01 
+// // console.log( myName + repCount + "Value")
+
+// console.log(`Hello my name is ${myName} and my repo count is ${repCount}.`);
+
+const gameName = new String('Prem_Malhotra')
+
+// console.log(gameName[0]);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const anotherString = gameName.slice(-12,3)
+console.log(anotherString);
+
+const newStringOne = "  prem  "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
