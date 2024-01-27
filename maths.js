656ca27d4acbb8567e2169f5b278e35fc92e43b6
Prem Malhotra <malhotra.prem290903@gmail>
2024-01-27 18:38:06 +0530

    New File

--- maths.js
+++
@@ -1,4 +0,0 @@
-min = 1
-max = 6
-
-console.log(Math.floor(Math.random()*(max - min + 1)+ min));
---
+++ maths_days.js
@@ -0,0 +1,28 @@
+// min = 10 
+// max = 100
+
+// console.log(Math.floor(Math.random()* (max - min) + min));
+
+// let newDate = new Date()
+// console.log(Date());
+// console.log(newDate.toISOString())
+// console.log(newDate.toJSON())
+// console.log(newDate.toLocaleDateString())
+// console.log(newDate.toLocaleTimeString())
+// console.log(newDate.toString())
+
+// let mynewDate = new Date("01-27-24")
+// console.log(mynewDate.toDateString());
+// console.log(typeof mynewDate);
+
+// let timestamp = Date.now()
+// // console.log(timestamp);
+// // console.log(timestamp.getTime());
+// console.log(Math.floor(Date.now()/1000))
+
+let newDate = new Date()
+newDate.toLocaleString("default", {
+        weekday: "narrow",
+        day: "numeric"
+})
+console.log(newDate.toLocaleString());
