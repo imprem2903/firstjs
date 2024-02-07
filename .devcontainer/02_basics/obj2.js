e09cb11cc01be673ebb4ee6fc30a9278302f7230
Prem Malhotra <malhotra.prem290903@gmail>
2024-02-07 17:49:27 +0530

    new info

--- .devcontainer/02_basics/obj2.js
+++ .devcontainer/02_basics/obj2.js
@@ -7,15 +7,68 @@
 
 // console.log(obj);
 
-const person = {
-    isHuman:{},
-    introduction : function(){
-        console.log(`My name is ${this.name} . Am I a human? ${this.isHuman}`);
+// const person = {
+//     isHuman:{},
+//     introduction : function(){
+//         console.log(`My name is ${this.name} . Am I a human? ${this.isHuman}`);
+//     }
+// }
+
+// const me = Object.create(person);
+// me.name = "Prem Malhotra";
+// me.isHuman = "Yes you are." 
+
+// me.introduction();
+
+const tinderUser = new Object()
+tinderUser.id = 1241
+tinderUser.name = "Prem"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const Sym = Symbol("Key1");
+
+
+ const userInfo = {
+    id : "124abx",
+    [Sym] : "Unique Symbol",
+    Details : {
+        firstName : "Prem",
+        lastName : "Malhotra"
     }
-}
+ }
+//  console.log(userInfo.Details);
 
-const me = Object.create(person);
-me.name = "Prem Malhotra";
-me.isHuman = "Yes you are." 
 
-me.introduction();
+const obj1 = {1:"a",2:"b"} //target 
+const obj2 = {3:"c", 4:"d"} //source
+// const obj3 = Object.assign({}, obj1, obj2)
+const obj3 = {...obj1,...obj2}
+
+// console.log(obj3);
+
+const users = [
+    {
+        id:1, email :"prem@mail"
+    },
+    {
+        id:2, email :"p@mail"
+    }, {
+        id:3, email :"pem@mail"
+    }, {
+        id:4, email :"pr@mail"
+    }, {
+        id:5, email :"pm@mail"
+    },
+]
+
+users[1].email;
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+// console.log(Object.fromEntries(tinderUser));
+
+
+console.log(tinderUser.hasOwnProperty("isLoggedIn"));
--- .devcontainer/02_basics/tempCodeRunnerFile.js
+++ .devcontainer/02_basics/tempCodeRunnerFile.js
@@ -1,13 +1,4 @@
-obj.greetings = function(){
-//     console.log("Hello , this is a function");
-// }
 
-// // console.log(obj.greetings())
-
-// // String Interpolation: (``)iske through ham koi bhi variable likh sakte hai using ${this.___}
-
-// obj.greeting2 = function(){
-//     console.log(`Hello , ${this.name} this is a function`);  
-// }
-// console.log(obj.greetings());
-// console.log(obj.greeting2());
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entri
