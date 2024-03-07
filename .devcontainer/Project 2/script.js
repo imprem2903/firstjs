42441bfc79307385504edba048c415e72a9a6fb3
Prem Malhotra <malhotra.prem290903@gmail>
2024-03-07 16:53:11 +0530

    Project 2 - BMI Calculator

--- .devcontainer/Project 2/script.js
+++ .devcontainer/Project 2/script.js
@@ -1,19 +1,26 @@
-const form = document.querySelector('form')
+const form = document.querySelector("form")
+form.addEventListener("submit" , function (event) {
+    event.preventDefault()
+     const height = parseInt(document.querySelector("#height").value)
+     const weight = parseInt(document.querySelector("#weight").value)
+     const results = document.querySelector('#results');    
 
-form.addEventListener('submit', function (event) {
-    event.preventDefault();
-
-   const height = parseInt(document.querySelector('#height').value);
-   const weight = parseInt(document.querySelector('#weight').value);
-   const results = document.querySelector('#results')
-
-   if ( height < 0 || isNaN(height) || height === '' ) {
-    results.innerHTML = `Please give a valid height ${height}`   
-    }else if ( weight < 0 || isNaN(weight) || weight === '' ) {
-        results.innerHTML = `Please give a valid weight ${weight}`   
-    } else{
-       const bmi =  (weight/((height*height)/1000)).toFixed(2)
-       results.innerHTML= `<span>${bmi}</span>`
+    if (height === '' || height<0 || isNaN(height)) {
+        results.innerHTML = "enter a valid height"
     }
-
+    else if (weight === '' || weight<0 || isNaN(weight)) {
+        results.innerHTML = "enter a valid weight"
+    }else{
+        const bmi = (weight/((height*height)/10000)).toFixed(2)
+        // results.innerHTML = `<span>${bmi}</span>`
+        if (bmi <18.6) {
+            results.innerHTML = `<span>${bmi}</span>, you are Under weight`
+        } else if (bmi>= 18.6 && bmi <= 24.9) {
+            results.innerHTML = `<span>${bmi}</span>, you are normal`
+        }
+        else {
+            results.innerHTML = `<span>${bmi}</span>, you are obese`
+        }
+    }
+    
 });
